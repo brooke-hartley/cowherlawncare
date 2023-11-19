1b5cc7c2c9385e066af062daee0d0c70faace22c
Brooke Hartley <82181143+brooke-hartley@users.noreply.github.com>
2023-11-19 12:56:54 -0500

    Added comment for function

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,6 +5,8 @@ import * as bootstrap from "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js
 import Rails from "@rails/ujs"
 Rails.start()
 
+/* Page scrolls to the top on refresh */
 document.addEventListener("turbo:load", function() {
     window.scrollTo(0, 0);
   });
+
